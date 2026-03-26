f387eaa6a2701796b38f55836d1947c109ac75be
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-03-25 21:44:07 -0300

    refactor(hook): consolidate states into a single useState object

--- src/hooks/useRoadmapData.js
+++ src/hooks/useRoadmapData.js
@@ -1,49 +1,53 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-export function useRoadmapData(name){
-    
-      const [renderData, setRenderData] = useState({});
-      const [error, setError] = useState(false);
-      const [loading, setLoading] = useState(true);
-     const [renderTopic, setRenderTopic] = useState([])
-     const [progress,setProgress] = useState([])
-    
-      useEffect(() => {
-        async function fetchLang() {
-          try {
-            const resRoadmap = await fetch(
-              `http://localhost:3000/roadmaps/lang?name=${name} `,
-            );
-            const roadmapData = await resRoadmap.json();
-            setRenderData(roadmapData);
-    
-            const resTopic = await fetch(
-              `http://localhost:3000/newTopics/roadmap?roadmapName=${name} `
-            )
-            const topicData = await resTopic.json()
-            setRenderTopic(topicData)
-    
-            const token = localStorage.getItem('token')
-            const resProgress = await axios.get(`http://localhost:3000/userProgress/roadmap/${roadmapData._id}`,
-              { headers: { Authorization: `Bearer ${token}` } })
-             setProgress(resProgress.data);
-    
-          } catch (err) {
-            console.error(err);
-            setError(true);
-          } finally {
-            setLoading(false);
-          }
-         }
-      
-        fetchLang();
-      }, [name, location.pathname]);
-       return {
-        renderData, 
-        renderTopic,   
-        progress,
-        loading,
-        error
-  };
+export function useRoadmapData(name) {
+  const [state, setState] = useState({
+    status: "loading",
+    error: null,
+    data: null,
+    topics: [],
+    progress: [],
+  });
+
+  useEffect(() => {
+    async function fetchLang() {
+      try {
+        const resRoadmap = await fetch(
+          `http://localhost:3000/roadmaps/lang?name=${name}`
+        );
+        const roadmapData = await resRoadmap.json();
+
+        const resTopic = await fetch(
+          `http://localhost:3000/newTopics/roadmap?roadmapName=${name}`
+        );
+        const topicData = await resTopic.json();
+
+        const token = localStorage.getItem("token");
+        const resProgress = await axios.get(
+          `http://localhost:3000/userProgress/roadmap/${roadmapData._id}`,
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+
+        setState({
+          status: "success",
+          data: roadmapData,
+          topics: topicData,
+          progress: resProgress.data,
+        });
+      } catch (err) {
+        setState({
+          status: "failed",
+          data: null,
+          error: err,
+          topics: null,
+          progress: null,
+        });
+      }
+    }
+
+    fetchLang();
+  }, [name]);
+
+  return state;
 }
