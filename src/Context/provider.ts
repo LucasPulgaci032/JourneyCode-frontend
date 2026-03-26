9ea247877cf80c9007b8ed0382b39016ad6492e0
Lucas Pulgaci <lucaspac032@hotmail.com>
2026-03-25 21:39:19 -0300

    chore: rename Context and MyRoadmaps folders for better structure

--- src/Context/provider.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-
-export type ContextType = {
-    color: String;
-    changeColor : () => void
-}
-
-export const Provider = createContext<ContextType | null>(null);
--- src/Pages/AboutPage/index.jsx
+++
@@ -1,7 +0,0 @@
-export function AboutPage() {
-  return (
-    <div>
-      <h1>oi</h1>
-    </div>
-  );
-}
